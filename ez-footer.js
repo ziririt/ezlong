413ac517f916aed6e0427383f45268dc3e1abc14
ezlong <ziririt@gmail.com>
2026-06-17 22:30:08 +0900

    feat: 투자유형 진단 페이지 타이틀·SEO/AEO 강화, nav·footer 순서 조정

--- ez-footer.js
+++ ez-footer.js
@@ -89,8 +89,8 @@
               '<a href="/retirement-calculator.html">은퇴 목표 역산 계산기</a>' +
               '<a href="/backtest.html">몬테카를로 포트폴리오 시뮬레이터</a>' +
               '<a href="/risk-diagnostic.html">투자 행동 자가진단</a>' +
+              '<a href="/stock-personality-quiz.html">MBTI로 찾는 맞춤 투자 포트폴리오</a>' +
               '<a href="/auto-dca-guide.html">자동 적립식 매수 가이드</a>' +
-              '<a href="/stock-personality-quiz.html">투자유형 자가진단</a>' +
             '</div>' +
           '</div>' +
         '</div>' +
