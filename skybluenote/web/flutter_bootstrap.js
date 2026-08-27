3069df2b540dd3a15381eee340a189120a16455e
ziririt <ziririt@gmail.com>
2026-08-27 11:38:11 +0900

    스카이블루 노트 웹 2.9.9 — 편집 화면이 남의 글을 도로 덮던 것 수정

--- skybluenote/web/flutter_bootstrap.js
+++ skybluenote/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3202203054" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2922641791" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
