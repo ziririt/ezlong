0404d0a41c4d88782ad8bd8e8dada60fe66df2ea
ziririt <ziririt@gmail.com>
2026-08-28 01:19:29 +0900

    Skyblue Note 웹앱 3.8.0 (202)

--- skybluenote/web/flutter_bootstrap.js
+++ skybluenote/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "394047940" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "685959538" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
