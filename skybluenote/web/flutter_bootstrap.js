a5f48f60257362725804b84e9b1b4a3d77245cc0
ziririt <ziririt@gmail.com>
2026-08-31 10:53:20 +0900

    skybluenote: 3.14.1 웹 + 소개 페이지(웹앱 먼저, 기기 그림 높이 제한, 문의 주소 교체)

--- skybluenote/web/flutter_bootstrap.js
+++ skybluenote/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1027568534" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3762612462" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
