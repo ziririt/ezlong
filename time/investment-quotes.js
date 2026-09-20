21a11ceabf37de718ddb734083e7156193da70b5
ziririt <ziririt@gmail.com>
2026-09-21 04:04:07 +0900

    content: 투자명언 자동수집 백로그 반영 (1건, Seth Klarman)

--- time/investment-quotes.js
+++ time/investment-quotes.js
@@ -10242,5 +10242,12 @@ window.investmentQuotes = [
     "title": "비커밍 (Becoming)",
     "author": "미셸 오바마 (Michelle Obama)",
     "genre": "literature"
+  },
+  {
+    "english": "Value investing is at its core the marriage of a contrarian streak and a calculator.",
+    "text": "가치투자란 본질적으로 반골 기질과 계산기의 결합이다.",
+    "title": "마진 오브 세이프티 (Margin of Safety)",
+    "author": "세스 클라만 (Seth Klarman)",
+    "category": "mindset"
   }
 ];
